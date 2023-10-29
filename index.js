eb09b16e502460f068e353a2d155893076895a9a
Kanav <66438237+kanjurer@users.noreply.github.com>
2023-10-29 18:26:53 -0400

    final

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const answer = "EGO"; // Define the correct answer here
+const answer = "jdeangelis79"; // Define the correct answer here
 
 document.getElementById("checkButton").addEventListener("click", function () {
     const userAnswer = document.getElementById("answerInput").value.toUpperCase();
     const resultElement = document.getElementById("result");
 
     if (userAnswer === answer) {
-        resultElement.textContent = "Correct! The answer is: " + answer;
+        resultElement.textContent = "https://dalu-my.sharepoint.com/:f:/r/personal/kn947171_dal_ca/Documents/halloween2023/winner?csf=1&web=1&e=2WfMS2";
         resultElement.style.color = "green";
     } else {
-        resultElement.textContent = "Incorrect. Try again.";
+        resultElement.textContent = "मौत मौत मौत मौत मौत मौत मौत मौत मौत मौत मौत";
         resultElement.style.color = "red";
     }
 
@@ -16,4 +16,4 @@ document.getElementById("checkButton").addEventListener("click", function () {
 });
 
 // Customize the puzzle text here
-document.getElementById("puzzle").textContent = "I am a three-letter word...";
+document.getElementById("puzzle").textContent = "the person who posted the last comment on  August 24, 2013 at 12:00 am on dalonline website";
